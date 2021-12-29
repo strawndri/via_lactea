8c3187d990d06e1413d480f03585d3cd483c8e74
Andrieli Luci <andrieliluci@gmail.com>
2021-12-29 18:44:20 -0300

    :iphone: Responsive layout (mobile): header

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -1,13 +1,16 @@
 const btnAccessibility = document.getElementById("btnAccessibility");
-const btnElements = document.querySelectorAll(".btn")
+const btnElements = document.querySelectorAll(".btn, .buttons-block")
+const titleBlockElement = document.getElementById("titleBlock")
 
 
 btnAccessibility.addEventListener('click', () => {
 
     for (var i = 0; i < btnElements.length; i++) {
 
-        btnElements[i].classList.toggle('enabled')
+        btnElements[i].classList.toggle('buttons-enabled')
     }
+
+    titleBlockElement.classList.toggle('hidden')
 
 });
 
