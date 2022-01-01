81f1ff8e6ac1bcf6b8426f253c1e70588fa38d4d
Andrieli Luci <andrieliluci@gmail.com>
2022-01-01 20:14:14 -0300

    :construction: Work in progress: add dark mode

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -1,5 +1,3 @@
-// import size from "../scss/helpers/_breakpoints.scss";
-
 const btnAccessibility = document.getElementById("btnAccessibility");
 const btnElements = document.querySelectorAll(".btn");
 const buttonsBlockElement = document.getElementById("buttonsBlock");
@@ -7,23 +5,24 @@ const titleBlockElement = document.getElementById("titleBlock");
 
 var size = 576;
 
-btnAccessibility.addEventListener('click', () => {
+btnAccessibility.addEventListener("click", () => {
 
     for (var i = 0; i < btnElements.length; i++) {
 
-        btnElements[i].classList.toggle('buttons-enabled')
+        btnElements[i].classList.toggle("buttons-enabled")
     }
 
     if (window.innerWidth < size) {
-        titleBlockElement.classList.toggle('hidden')
-        buttonsBlockElement.classList.toggle('buttons-enabled')
+        titleBlockElement.classList.toggle("hidden")
+        buttonsBlockElement.classList.toggle("buttons-enabled")
     }
 
 });
 
 
 const btnCheckBox = document.getElementById("checkbox");
+const element = document.body
 
 btnCheckBox.addEventListener("change", () => {
-
+    element.classList.toggle("darkmode")
 })
