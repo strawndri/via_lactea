b99f85b3c9b8025fadd4e2a59450cc46d0fef0d6
Andrieli Luci <andrieliluci@gmail.com>
2022-01-04 19:02:47 -0300

    Change font icons (svg path to background-image)

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -1,23 +1,23 @@
-// const btnAccessibility = document.getElementById("btnAccessibility");
-// const btnElements = document.querySelectorAll(".btn");
-// const buttonsBlockElement = document.getElementById("buttonsBlock");
-// const titleBlockElement = document.getElementById("titleBlock");
+const btnAccessibility = document.getElementById("btnAccessibility");
+const btnElements = document.querySelectorAll(".btn");
+const buttonsBlockElement = document.getElementById("buttonsBlock");
+const titleBlockElement = document.getElementById("titleBlock");
 
-// var size = 576;
+var size = 576;
 
-// btnAccessibility.addEventListener("click", () => {
+btnAccessibility.addEventListener("click", () => {
 
-//     for (var i = 0; i < btnElements.length; i++) {
+    for (var i = 0; i < btnElements.length; i++) {
 
-//         btnElements[i].classList.toggle("buttons-enabled")
-//     }
+        btnElements[i].classList.toggle("buttons-enabled")
+    }
 
-//     if (window.innerWidth < size) {
-//         titleBlockElement.classList.toggle("hidden")
-//         buttonsBlockElement.classList.toggle("buttons-enabled")
-//     }
+    if (window.innerWidth < size) {
+        titleBlockElement.classList.toggle("hidden")
+        buttonsBlockElement.classList.toggle("buttons-enabled")
+    }
 
-// });
+});
 
 
 // const btnCheckBox = document.getElementById("checkbox");
