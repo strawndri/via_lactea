f09fa5df38f75a32bccd6422c62f7c1fa7fd666f
Andrieli Luci <andrieliluci@gmail.com>
2022-01-05 18:22:11 -0300

    add a new class (disabled), that change the possibility to click (btn-font configuration is finished)

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -1,20 +1,16 @@
-// const btnAccessibility = $("#btnAccessibility");
 const btnElements = $(".btn");
-// const buttonsBlockElement = $("#buttonsBlock");
-// const titleBlockElement = $("#titleBlock");
-
 var size = 576;
 
 $("#btnAccessibility").click(function() {
 
     for (var i = 0; i < btnElements.length; i++) {
 
-        $(".btn").toggleClass("buttons-enabled");
+        $(".btn").toggleClass("visible");
     }
 
     if (window.innerWidth < size) {
         $("#titleBlock").toggleClass("hidden")
-        $("#buttonsBlock").toggleClass("buttons-enabled")
+        $("#buttonsBlock").toggleClass("visible")
     }
 
 });
@@ -40,10 +36,12 @@ $(".btn-font").click (function(e) {
     var fontSize = parseInt($("main").css("font-size"))
 
     if (e.target.id == "fontUp") {
-        fontSize < 24 ?  $("main").css("font-size", fontSize + 1) : console.log('Stop!')
+        fontSize < 24 ?  ($("main").css("font-size", fontSize + 1),
+         $("#fontDown").removeClass("disabled")) : $("#fontUp").addClass("disabled")
     }
 
     else {
-        fontSize > 14 ?  $("main").css("font-size", fontSize - 1) : console.log('Stop!')
+        fontSize > 14 ?  ($("main").css("font-size", fontSize - 1),
+            $("#fontUp").removeClass("disabled")) : $("#fontDown").addClass("disabled");
     }    
 })
